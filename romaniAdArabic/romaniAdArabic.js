1bfaa64a18de20659bd9c31acee58ee8214507f0
Maso283 <justin.roberts.21221@protonmail.com>
2021-03-18 15:11:22 -0500

    Work on romaniAdArabic has begun.

---
+++ romaniAdArabic/romaniAdArabic.js
@@ -0,0 +1,25 @@
+function convertToRoman(num) {
+
+	//Roman numerals to Arabic numerals dictionary
+	const romaniAdArabic {
+
+		I:1,
+		V:5,
+		X:10,
+		L:50,
+		C:100,
+		D:500,
+		M:1000
+
+	};
+
+	//convert integer to a string
+	num = num.toString()
+
+	//Split string into an array of strings
+
+	//console.log(typeof(num));
+
+}
+
+convertToRoman(15);
