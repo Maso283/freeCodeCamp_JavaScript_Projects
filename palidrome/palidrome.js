a3526d3731dcf3dc79259ea0e47e8e2ff316909b
Maso283 <justin.roberts.21221@protonmail.com>
2021-03-18 11:14:37 -0500

    Began palidrome code

---
+++ palidrome/palidrome.js
@@ -0,0 +1,20 @@
+function palindrome(str) {
+
+  //Remove special characters from string
+
+  //converts string to uppercase or lowercase
+
+  //stores string in a variable
+
+  //reverses string and stores in a variable
+
+  //if checker  
+
+  return true;
+}
+
+
+
+
+//This reverses strings
+//return str.split("").reverse().join("");
