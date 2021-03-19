012a54f5ee8bc9d1068a587618ff8dda5e176d3c
Maso283 <justin.roberts.21221@protonmail.com>
2021-03-18 20:21:56 -0500

    [18.03.21] Calling it for the day. Must figure out how to match the split array with the romaniAdArabic object.

--- romaniAdArabic/romaniAdArabic.js
+++ romaniAdArabic/romaniAdArabic.js
@@ -3,19 +3,18 @@ function convertToRoman(num) {
 
 
 	//Initializing the variables we will need
-	let numberSplit = [];
 	let zeroes = ""
 
 	//Roman numerals to Arabic numerals dictionary
 	const romaniAdArabic = {
 
-		I:1,
-		V:5,
-		X:10,
-		L:50,
-		C:100,
-		D:500,
-		M:1000
+		I:'1',
+		V:'5',
+		X:'10',
+		L:'50',
+		C:'100',
+		D:'500',
+		M:'1000'
 
 	};
 
@@ -32,27 +31,15 @@ function convertToRoman(num) {
 	//Iterare through array in reverse
 	for (counter = num.length - 1; counter >= 0; counter--) {
 
-		
-
-		//append zero onto zeroes
-		//append zero(s) onto array item
-
-		//alert(num)
-
-		alert(zeroes);
-
+		num[counter] += zeroes;
+		alert(num[counter]);
 		zeroes += "0";
 
 	};
 
-	/*For every number iterated over in the array after
-	the last one in the array, append an increment 
-	of zeros onto it
-
-	i.e.
-
-	["1", "5", "5", "8"] :Index
-	  3    2    1    0   :Zeroes*/
+	//for (counter = num.length - 1; counter >= 0; counter--) {
+	//	for 
+	//}
 
 	console.log(num);
 
